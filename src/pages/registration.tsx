d8006a66834131d3a528fc58b494a3fde053d3e4
Lauren <lauren.k.muhar@gmail.com>
2020-11-16 21:45:45 +1100

    clean up lint errors

--- src/pages/registration.tsx
+++ src/pages/registration.tsx
@@ -2,8 +2,22 @@ import { Box, Button, FormControl, Heading, Input } from '@chakra-ui/core';
 import { NextPage } from 'next';
 import React from 'react';
 import DefaultLayout from '../layouts/default.layout';
+import { useForm } from 'react-hook-form';
+import to from 'await-to-js';
+import Axios from 'axios';
 
 const IndexPage: NextPage = () => {
+  const { handleSubmit, register } = useForm();
+  const onSubmit = async (data) => {
+    const [err, res] = await to(Axios.post(`http://localhost:${process.env.PORT || 3000}/api/user/register`, data));
+    if (err) {
+      // console.log(err);
+    }
+
+    if (res) {
+      // console.log('Success');
+    }
+  };
   return (
     <DefaultLayout>
       <Box p={8} maxWidth="500px" borderWidth={1} borderRadius={8} boxShadow="lg">
@@ -11,15 +25,15 @@ const IndexPage: NextPage = () => {
           <Heading>Register</Heading>
         </Box>
         <Box my={4} textAlign="left">
-          <form>
-            <FormControl>
-              <Input id="email" type="email" placeholder="Email" />
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <FormControl isRequired>
+              <Input name="email" id="email" type="email" placeholder="Email" ref={register} />
             </FormControl>
-            <FormControl mt={6}>
-              <Input id="username" type="text" placeholder="Username" />
+            <FormControl mt={6} isRequired>
+              <Input name="username" id="username" type="text" placeholder="Username" ref={register} />
             </FormControl>
-            <FormControl mt={6}>
-              <Input id="password" type="password" placeholder="Password" />
+            <FormControl mt={6} isRequired>
+              <Input name="password" id="password" type="password" placeholder="Password" ref={register} />
             </FormControl>
             <Button type="submit" width="full" mt={4}>
               Register
