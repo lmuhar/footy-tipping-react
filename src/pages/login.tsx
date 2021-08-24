7b46d7a680762764c3a20414c4a763b8c5bfe494
Lauren M <lauren.k.muhar@gmail.com>
2021-08-24 17:00:26 +1000

    add

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -51,7 +51,7 @@ const IndexPage: NextPage = (_props) => {
   const { control, handleSubmit } = useForm<FormValues>();
   const onSubmit = async (data) => {
     const [err, res] = await to(
-      Axios.post<UserToken>(`https://0.0.0.0:${process.env.PORT || 80}/api/user/login`, data),
+      Axios.post<UserToken>(`https://localhost${process.env.PORT || 80}/api/user/login`, data),
     );
     if (err) {
       console.log(err);
