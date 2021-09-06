3d3b5c94337e0cb22a996227ccd9dbef10411852
Lauren M <lauren.k.muhar@gmail.com>
2021-09-06 14:40:21 +1000

    add round apis

---
+++ src/models/round.model.ts
@@ -0,0 +1,6 @@
+export interface IRound {
+    id?: string;
+    dateStart: Date;
+    dateEnd: Date;
+    roundNumber: number;
+}
---
+++ src/pages/api/round/create.ts
@@ -0,0 +1,27 @@
+import { IRound } from './../../../models/round.model';
+import { PrismaClient } from '@prisma/client';
+import to from 'await-to-js';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+
+export async function createRoundService(data: IRound): Promise<[Error, IRound]> {
+    const prisma = new PrismaClient();
+
+    if (!data.roundNumber || !data.dateStart || !data.dateEnd) return [new Error('Something went wrong creating the round'), null];
+
+    const [err, round] = await to(prisma.round.create({data}));
+
+    if (err) return [new Error('Something went wrong creating the round'), null];
+
+    if (!round) return [new Error('Something went wrong creating the round'), null];
+
+    return [null, data];
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
+    const [err, round] = await createRoundService(req.body);
+
+    if (err) return res.status(500).json(err);
+
+    return res.status(200).json(round);
+}
