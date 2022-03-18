27832aa4bdd024cc2f7e1db4746c66da3b8ac805
Lauren Muhar <A140793@agl.com.au>
2022-03-18 17:20:07 +1100

    display tips

--- src/components/section/view-tips-screen.tsx
+++ src/components/section/view-tips-screen.tsx
@@ -1,5 +1,10 @@
-import { makeStyles } from '@material-ui/core';
+import { Box, Grid, makeStyles } from '@material-ui/core';
+
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import _ from 'lodash';
 
 interface CompProp {
     tips: any
@@ -13,15 +18,32 @@ const useStyles = makeStyles(() => ({
 
 const ViewTipsScreen: React.FunctionComponent<CompProp> = ({tips}) => {
     const classes = useStyles();
+    const sortedTips = _.orderBy(tips, 'game.startDateTime')
+    const userTips = _.groupBy(sortedTips, 'user.username');
+    const keys = Object.keys(userTips);
 
     return (
-    <div>
-        {tips.map((tip) => {
-        const fieldName = `tip${tip.id}`;
-        return (<div key={fieldName}>
-            <span className={tip?.game.homeTeam?.name === tip?.selectedTip.name ? classes.selected : null}>{tip?.game.homeTeam?.name}</span> - V - <span className={tip?.game.awayTeam?.name === tip?.selectedTip.name ? classes.selected : null}>{tip?.game.awayTeam?.name}</span></div>)
+      <Box sx={{flexGrow: 1}}>
+        <Grid container spacing={3}>
+        {keys.map((key, index) => {
+          const fieldName = key;
+          return (
+          <Grid key={fieldName + index} item xs={8}>
+            <Card>
+              <CardContent>
+                <Typography variant="h5">{key}</Typography>
+              {userTips[key].map((tip) => {
+      const fieldName = `tip${tip.id}`;
+      return (<Typography variant="body2" key={fieldName}>
+          <span className={tip?.game.homeTeam?.name === tip?.selectedTip.name ? classes.selected : null}>{tip?.game.homeTeam?.name}</span> - V - <span className={tip?.game.awayTeam?.name === tip?.selectedTip.name ? classes.selected : null}>{tip?.game.awayTeam?.name}</span></Typography>)
+              })}
+              </CardContent>
+            </Card>
+          </Grid>
+          )
         })}
-    </div>
+        </Grid>
+      </Box>
     );
 }
 
