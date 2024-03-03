a51639ba165c4854cf09e3171d837f95f09179ff
Luke Bayliss <hello@lukebayliss.com>
2024-03-03 14:16:31 +1100

    little more work on ui

--- src/pages/admin/manage.tsx
+++ src/pages/admin/manage.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from 'next';
-import { Box, Button, Card, Divider, HStack, Heading, VStack } from '@chakra-ui/react';
+import { Box, Button, Card, Divider, HStack, Heading, Input, VStack } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import { api } from '~/utils/api';
 import { useSession } from 'next-auth/react';
 import { format, parseISO } from 'date-fns';
+import { ApplicationShell } from '~/layouts/application-shell';
 
 const LoginPage: NextPage = () => {
   const utils = api.useUtils();
@@ -19,48 +20,68 @@ const LoginPage: NextPage = () => {
 
   const createSeasonMutation = api.createSeason.useMutation();
 
-  const handleCreateSeason = () => {
-    createSeasonMutation.mutate({
-      year: '2024',
-      roundsCount: 24,
-      startWeek: '2024-04-07T00:00:00Z',
+  const handleCreateSeason = async () => {
+    const today = new Date();
+    await createSeasonMutation.mutateAsync({
+      year: format(today, 'yyyy'),
+      roundsCount: 1,
+      startWeek: today.toISOString(),
     });
-    void utils.fetchSeasons.invalidate();
+    await utils.fetchSeasons.invalidate();
   };
 
   return (
-    <Box color="black">
-      <Heading size="lg">Manage</Heading>
-
-      <Box>{JSON.stringify(seasons)}</Box>
-
-      <VStack p="12" alignItems="flex-start">
-        <Heading size="lg">Seasons</Heading>
-        {seasons?.map((season) => (
-          <Box key={season.id}>
-            <HStack>
-              <Heading size="md">{season.year} Season</Heading>
-              <Button colorScheme="blue" onClick={handleCreateSeason}>
-                Create new
-              </Button>
-            </HStack>
-            <Divider />
-            <VStack alignItems="flex-start">
-              {season.rounds.map((round) => (
-                <Card key={round.id} w="full" py="8" px="6">
-                  <Heading size="sm">Round {round.roundNumber}</Heading>
+    <ApplicationShell>
+      <Card px="6" py="3">
+        <Heading size="lg" mb="2">
+          Manage
+        </Heading>
+        <VStack alignItems="flex-start" w="full">
+          <HStack w="full">
+            <Heading size="lg">Seasons</Heading>
+            <Button colorScheme="blue" size="sm" ml="auto" onClick={handleCreateSeason}>
+              Create new
+            </Button>
+          </HStack>
+          <Divider />
+          {seasons?.map((season) => (
+            <Box key={season.id} w="full">
+              <HStack>
+                <Heading size="md" mb="2">
+                  {season.year} Season
+                </Heading>
+              </HStack>
+              <VStack alignItems="flex-start">
+                {season.rounds.map((round) => (
+                  <Box px="2" py="2" key={round.id} w="full" borderColor="gray.600" borderWidth="1px" rounded="md">
+                    <Heading size="sm">Round {round.roundNumber}</Heading>
+                    <VStack alignItems="flex-start">
+                      <HStack w="full">
+                        <HStack w="full">
+                          <Box>Start:</Box>
+                          <Input id="date-start" type="date" value={format(parseISO(round.dateStart), 'yyyy-MM-dd')} />
+                        </HStack>
+                        <HStack w="full">
+                          <Box>End:</Box>
+                          <Input id="date-start" type="date" value={format(parseISO(round.dateEnd), 'yyyy-MM-dd')} />
+                        </HStack>
+                      </HStack>
+                      <Box>{round._count.games} Games</Box>
+                      <Button>Add Game</Button>
+                    </VStack>
+                  </Box>
+                ))}
+                <Box w="full" py="8" px="6">
                   <HStack>
-                    <Box>{format(parseISO(round.dateStart), 'PPP')}</Box>
-                    <Box>{format(parseISO(round.dateEnd), 'PPP')}</Box>
-                    <Box>{round._count.games} Games</Box>
+                    <Button>Add Round</Button>
                   </HStack>
-                </Card>
-              ))}
-            </VStack>
-          </Box>
-        ))}
-      </VStack>
-    </Box>
+                </Box>
+              </VStack>
+            </Box>
+          ))}
+        </VStack>
+      </Card>
+    </ApplicationShell>
   );
 };
 
